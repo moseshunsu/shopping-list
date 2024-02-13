2a5fecbd725d2d647c8eb524eaf8a1437a827fed
Moses Hunsu <hunsumoses7@gmail.com>
2024-02-13 17:36:53 +0100

    Worked on the Item Component

--- src/components/ShoppingItems.jsx
+++ src/components/ShoppingItems.jsx
@@ -1,3 +1,5 @@
+import { MdEdit, MdDelete } from "react-icons/md";
+
 export default function ShoppingItems({ items }) {
   return (
     <div className="items">
@@ -11,9 +13,13 @@ export default function ShoppingItems({ items }) {
 function Item({ items }) {
   return items.map((item) => (
     <li key={item.id} className="item">
-      <input type="checkbox" />
-      <span>
+      <input type="checkbox" value={item.purchased} checked={item.purchased} />
+      <span style={item.purchased ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.name}
+      </span>
+      <span className="icons">
+        <MdEdit className="edit icon" />
+        <MdDelete className="delete icon" />
       </span>
     </li>
   ));
