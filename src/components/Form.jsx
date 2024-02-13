78596b0906df295476eaaa2be5462d39ec955d0e
Moses Hunsu <hunsumoses7@gmail.com>
2024-02-13 22:45:18 +0100

    Made the form dynamic to the UI

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 export default function Form({ onAddItem }) {
   const [quantity, setQuantity] = useState(1);
   const [name, setName] = useState("");
+  const [displayButton, setDisplayButton] = useState(true);
+  const [displayForm, setDisplayForm] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -24,34 +26,50 @@ export default function Form({ onAddItem }) {
     setName("");
   }
 
+  function handleDisplayForm() {
+    setDisplayButton((displayButton) => !displayButton);
+    setDisplayForm((displayForm) => !displayForm);
+  }
+
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="quantity">Quantity</label>
-        <input
-          type="number"
-          name="quantity"
-          id="quantity"
-          value={quantity}
-          onChange={(e) =>
-            setQuantity(e.target.value < 1 ? quantity : e.target.value)
-          }
-        />
-      </div>
+    <>
+      <button
+        className={displayButton ? "show" : "hidden"}
+        onClick={handleDisplayForm}
+      >
+        Add Item
+      </button>
+      <form
+        className={`${displayForm ? "show" : "hidden"} form`}
+        onSubmit={handleSubmit}
+      >
+        <div>
+          <label htmlFor="quantity">Quantity</label>
+          <input
+            type="number"
+            name="quantity"
+            id="quantity"
+            value={quantity}
+            onChange={(e) =>
+              setQuantity(e.target.value < 1 ? quantity : e.target.value)
+            }
+          />
+        </div>
 
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Item name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Item name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
 
-      <button>Add</button>
-    </form>
+        <button onClick={handleDisplayForm}>Add</button>
+      </form>
+    </>
   );
 }
